69876eee803ee0ca6ba53ecb22ecac515dfa1b5d
Christopher D. Chapman <chris@m3mnoch.com>
2021-11-18 13:59:07 -0800

    plan rotation: starting back up.

--- publish.sh
+++ publish.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+echo "a few words for the plan summary:"
+read summary
+
+date_daily=`date +"%Y-%m-%d"`
+echo "publishing plan to archive on ${date_daily}"
+
+cp README.md archive/${date_daily}-plan.md
+
 git add .
-git commit -m "plan update"
+git commit -m "plan rotation: ${summary}"
 git push
+
--- rotate.sh
+++ rotate.sh
@@ -12,7 +12,7 @@ git add archive
 git commit -m "plan rotation: ${summary}"
 git push
 
-./tweet.sh post new .plan file: ${summary} -- https://github.com/m3mnoch/.plan/blob/master/archive/${date_daily}-plan.md
+# ./tweet.sh post new .plan file: ${summary} -- https://github.com/m3mnoch/.plan/blob/master/archive/${date_daily}-plan.md
 
 printf "# an awesome .plan\n\n#### if you're looking for my previous days, check the archives.\n\n---\n\n" > README.md
 
